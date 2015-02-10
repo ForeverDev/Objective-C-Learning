faafa35041981a36d0c6a06863d22c05c86b15d0
David Wilson <david.wilson@govsacademy.org>
2015-02-10 16:39:52 -0500

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,15 @@
+#ifndef LearningOC_Person_h
+#define LearningOC_Person_h
+
+@interface Person: NSObject {
+    
+}
+
+- (void) pay: (int) amount;
+
+@property double paycheck;
+@property double balance;
+
+@end
+
+#endif
